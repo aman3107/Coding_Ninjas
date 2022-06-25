e0d7ea496bab1e96dcf30b803d33cdbe5723cb6f
Aman Mittal <amanmittalma@gmail.com>
2022-06-25 17:20:57 +0530

    All Prime Numbers

---
+++ Intro_CPP/prime_num.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << "Enter n " << endl;
+  cin >> n;
+  for (int i = 2; i <= n; i++)
+  {
+    bool is_prime = true;
+    for (int j = 2; j < i; j++)
+    {
+      if (i % j == 0)
+      {
+        is_prime = false;
+        break;
+      }
+    }
+    if (is_prime == true)
+      cout << i << endl;
+  }
+}
