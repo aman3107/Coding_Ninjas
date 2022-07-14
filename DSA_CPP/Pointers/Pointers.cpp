3daf16e26c7afe978492e0aa4884afc0912a8acd
Aman Mittal <amanmittalma@gmail.com>
2022-07-14 13:37:49 +0530

    Pointers Starting

---
+++ DSA_CPP/Pointers/Pointers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i = 10;
+  cout << &i << endl;
+  int *p = &i;
+  cout << p << endl;
+  float f = 10.2;
+  float *pf = &f;
+  cout << pf << endl;
+  double d = 122.32;
+  double *pd = &d;
+  cout << pd << endl;
+}
