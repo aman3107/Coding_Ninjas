7cddf8f59f541cf54af07a2310812293d41c9b56
Aman Mittal <amanmittalma@gmail.com>
2022-07-16 14:40:09 +0530

    GFG recursion n to 1

---
+++ DSA_CPP/Recursion1/gfgrecursion_n_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void fun(int n)
+{
+  if (n == 0)
+    return;
+  fun(n - 1);
+  cout << n << " ";
+}
+
+int main()
+{
+  fun(5);
+  cout << endl;
+  return 0;
+}
