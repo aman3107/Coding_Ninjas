1d55a4706e63598d2079913d7cc5fe1afef59457
Aman Mittal <amanmittalma@gmail.com>
2022-07-25 15:52:31 +0530

    All index of a number

---
+++ DSA_CPP/Recursion1/all_index_of_number.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int all_index(int arr[], int n, int x, int output[])
+{
+  if (n == 0)
+    return 0;
+
+  int ans = all_index(arr + 1, n - 1, x, output);
+  if (arr[0] == x)
+  {
+    output[ans] = n;
+    return ans + 1;
+  }
+  return ans;
+}
+
+int main()
+{
+  int n;
+  cout << "Enter n : " << endl;
+  cin >> n;
+  int *arr;
+  arr = new int[n];
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
+
+  int *output;
+  output = new int[n];
+
+  int x;
+  cout << "Enter x : " << endl;
+  cin >> x;
+
+  int ans = all_index(arr, n, x, output);
+  cout << ans << endl;
+
+  for (int i = 0; i < ans; i++)
+    cout << output[i] << " ";
+  cout << endl;
+}
