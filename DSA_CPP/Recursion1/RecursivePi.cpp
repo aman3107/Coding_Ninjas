3d4290377a994a38a1f165522c1d94bcff29c531
Aman Mittal <amanmittalma@gmail.com>
2022-07-29 22:08:38 +0530

    Recursive Pi

---
+++ DSA_CPP/Recursion1/RecursivePi.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void replacePi(char ch[])
+{
+  if (ch[0] == '\0')
+    return;
+  if (ch[0] == 'p' && ch[1] == 'i')
+  {
+    ch[0] = '3';
+    ch[1] = '.';
+    int len = strlen(ch);
+    for (int i = len + 2; i > 1; i--)
+    {
+      ch[i] = ch[i - 2];
+    }
+    ch[2] = '1';
+    ch[3] = '4';
+  }
+  replacePi(ch + 1);
+}
+
+int main()
+{
+  char input[1000];
+  cin.getline(input, 1000);
+  replacePi(input);
+  cout << input << endl;
+}
