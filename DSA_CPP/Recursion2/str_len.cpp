d13f563f1776b64e2521fbaaf5aa2481405cdf47
Aman Mittal <amanmittalma@gmail.com>
2022-07-30 19:32:06 +0530

    Update File

--- DSA_CPP/Recursion2/str_len.cpp
+++ DSA_CPP/Recursion2/str_len.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
+
+void RemoveX(char ch[])
+{
+  if (ch[0] == '\0')
+    return;
+  if (ch[0] == 'x')
+  {
+    int len = strlen(ch);
+    for (int i = 0; i < len; i++)
+    {
+      ch[i] = ch[i + 1];
+    }
+    ch = ch - 1;
+  }
+  RemoveX(ch + 1);
+}
 
 int length(char ch[])
 {
@@ -12,6 +29,8 @@ int main()
 {
   char ch[1000];
   cin >> ch;
-  int len = length(ch);
-  cout << len << endl;
+  // int len = length(ch);
+  // cout << len << endl;
+  RemoveX(ch);
+  cout << ch << endl;
 }
