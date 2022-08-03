c502259aa04e8d575b7b019f02fe9df0cab4c9f0
Aman Mittal <amanmittalma@gmail.com>
2022-08-03 13:19:13 +0530

    Print Keypad Combinations Code

---
+++ DSA_CPP/Recursion2/Print_Keypad_Combinations_Code.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+void printKeypad(int num, string output = "")
+{
+  string input;
+  if (num == 0)
+  {
+    cout << output << endl;
+    return;
+  }
+
+  int n = num % 10;
+
+  switch (n)
+  {
+  case 2:
+    input = "abc";
+    break;
+  case 3:
+    input = "def";
+    break;
+  case 4:
+    input = "ghi";
+    break;
+  case 5:
+    input = "jkl";
+    break;
+  case 6:
+    input = "mno";
+    break;
+  case 7:
+    input = "pqrs";
+    break;
+  case 8:
+    input = "tuv";
+    break;
+  case 9:
+    input = "wxyz";
+    break;
+  }
+  for (int i = 0; i < input.size(); i++)
+  {
+    printKeypad(num / 10, input[i] + output);
+  }
+}
+
+int main()
+{
+  int num;
+  cout << "Enter num : " << endl;
+  cin >> num;
+  printKeypad(num);
+}
