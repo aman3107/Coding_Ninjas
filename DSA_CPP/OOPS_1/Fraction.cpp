a933ef76816a85c157cf49e0eba970b272099bc7
Aman Mittal <amanmittalma@gmail.com>
2022-08-11 16:12:55 +0530

    Fraction class

---
+++ DSA_CPP/OOPS_1/Fraction.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+class Fraction
+{
+  int numerator;
+  int denominator;
+
+public:
+  Fraction(int numerator, int denominator)
+  {
+    this->numerator = numerator;
+    this->denominator = denominator;
+  }
+
+  void print()
+  {
+    cout << numerator << "/" << denominator << endl;
+  }
+
+  void simplfy()
+  {
+    int gcd = 1;
+    int small = min(numerator, denominator);
+    for (int i = 1; i <= small; i++)
+    {
+      if (numerator % i == 0 && denominator % i == 0)
+      {
+        gcd = i;
+      }
+    }
+    numerator = numerator / gcd;
+    denominator = denominator / gcd;
+  }
+
+  void add(const Fraction &f2)
+  {
+    int lcm = this->denominator * f2.denominator;
+    int x = lcm / this->denominator;
+    int y = lcm / f2.denominator;
+    int num = x * (this->numerator) + (y * f2.numerator);
+    this->numerator = num;
+    this->denominator = lcm;
+  }
+
+  void multiply(Fraction const &f2)
+  {
+    this->numerator = this->numerator * f2.numerator;
+    this->denominator = this->denominator * f2.denominator;
+  }
+};
---
+++ DSA_CPP/OOPS_1/FractionUse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Fraction.cpp"
+using namespace std;
+
+int main()
+{
+  Fraction f1(10, 2);
+  Fraction f2(15, 4);
+  f1.multiply(f2);
+  f1.simplfy();
+  f1.print();
+}
