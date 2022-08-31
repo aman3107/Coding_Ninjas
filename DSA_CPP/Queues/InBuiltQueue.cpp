6f26247e1f6f437c53c7661771145ac276ab236b
Aman Mittal <amanmittalma@gmail.com>
2022-08-31 16:10:07 +0530

    Update Code:

--- DSA_CPP/Queues/InBuiltQueue.cpp
+++ DSA_CPP/Queues/InBuiltQueue.cpp
@@ -8,8 +8,17 @@ int main()
   q1.push(10);
   q1.push(20);
   q1.push(30);
+  q1.push(40);
+  q1.push(50);
+  q1.push(60);
   q1.pop();
   cout << q1.front() << endl;
   cout << q1.size() << endl;
   cout << q1.empty() << endl;
+
+  while (!q1.empty())
+  {
+    cout << q1.front() << endl;
+    q1.pop();
+  }
 }
